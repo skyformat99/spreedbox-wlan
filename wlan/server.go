9c190cd486295d17808f6d426e4ac355bfc6b0bd
Simon Eisenmann <simon@struktur.de>
2015-12-01 19:31:48 +0100

    Implemented wlan.interfaces bus.

--- wlan/server.go
+++ wlan/server.go
@@ -1,8 +1,11 @@
 package wlan
 
 import (
+	"golang.struktur.de/spreedbox/spreedbox-conf/conf"
 	"golang.struktur.de/spreedbox/spreedbox-go/bus"
+	"golang.struktur.de/spreedbox/spreedbox-network/network"
 	"log"
+	"net"
 	"os"
 	"os/signal"
 	"syscall"
@@ -38,5 +41,33 @@ func (s *Server) Serve() (err error) {
 
 func (s *Server) interfaces(subject, reply string, msg *InterfacesRequest) {
 	log.Println("interfaces", subject, reply, msg.Names)
+	wls := &WlanSettings{}
 
+	// devices
+	if interfaces, err := net.Interfaces(); err == nil {
+		interfacesResult := make(map[string]*WlanInterface)
+
+		for _, i := range interfaces {
+			if !network.IsInterfaceEthernet(i.Name) || !network.IsInterfaceWifi(i.Name) {
+				continue
+			}
+			wi := &WlanInterface{
+			// TODO(longsleep): Add details
+			}
+			interfacesResult[i.Name] = wi
+		}
+		wls.Interfaces = interfacesResult
+
+	} else {
+		log.Println("failed to read interfaces", err)
+	}
+
+	if reply != "" {
+		replyData, err := conf.NewDataReply(wls, nil)
+		if err != nil {
+			log.Println("failed to create interfaces reply", err)
+			return
+		}
+		s.ec.Publish(reply, replyData)
+	}
 }
--- wlan/wlan.go
+++ wlan/wlan.go
@@ -8,6 +8,17 @@ const (
 	BUS_WLAN_SUBJECT = "wlan"
 )
 
+type WlanSettings struct {
+	Interfaces map[string]*WlanInterface `json:"devices,omitempty"`
+}
+
+type WlanInterface struct {
+	Protocol string `json:"protocol"`
+	Ssid     string `json:"ssid"`
+	Channel  string `json:"channel"`
+	Quality  int8   `json:"quality"`
+}
+
 type InterfacesRequest struct {
 	Names []string `json:"names,omitempty"`
 }
