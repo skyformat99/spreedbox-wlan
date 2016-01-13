083ef2a5ca9e21a8c21591be1ad2baac3678b454
Simon Eisenmann <simon@struktur.de>
2016-01-13 17:57:22 +0100

    Prepare hotspot.

---
+++ doc/setuphotspot.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+
+set -e
+
+DEVICE=wlan0
+SSID=spreedbox
+NETWORK_PREFIX=192.168.43
+
+HOSTAPD=$(which hostapd)
+UDHCPD=$(which udhcpd)
+
+TMPDIR=$(mktemp -d)
+UDHCPD_PID=
+
+cleanup () {
+	trap "" EXIT
+	echo "Stopping ..."
+	if [ -e hostapd.pid ]; then
+		kill -TERM $(cat hostapd.pid) 2>/dev/null || true
+	fi
+	kill -TERM ${UDHCPD_PID} 2>/dev/null || true
+	rm -rf ${TMPDIR}
+	echo "Shutting down device ${DEVICE} ..."
+	ifconfig ${DEVICE} down
+	echo "Done."
+}
+trap "cleanup" INT QUIT TERM EXIT
+
+startdevice () {
+	echo "Starting device ${DEVICE} ..."
+	ifconfig ${DEVICE} ${NETWORK_PREFIX}.1/24 up
+}
+
+hostapd () {
+	cat >hostapd.conf <<EOL
+interface=${DEVICE}
+ssid=${SSID}
+driver=rtl871xdrv
+hw_mode=g
+channel=11
+auth_algs=3
+wmm_enabled=1
+ap_isolate=1
+EOL
+	echo "Starting hostapd ..."
+	${HOSTAPD} -B -P hostapd.pid hostapd.conf
+}
+
+udhcpd () {
+	cat >udhcpd.conf <<EOL
+start		${NETWORK_PREFIX}.20
+end			${NETWORK_PREFIX}.254
+interface	${DEVICE}
+lease_file	udhcpd.leases
+auto_time	0
+opt		domain	local
+opt		subnet	255.255.255.0
+opt		router	${NETWORK_PREFIX}.1
+opt		dns		${NETWORK_PREFIX}.1
+EOL
+	echo "Starting udhcpd ..."
+	touch udhcpd.leases
+	${UDHCPD} -f udhcpd.conf &
+	UDHCPD_PID=$!
+}
+
+cd ${TMPDIR}
+
+startdevice
+hostapd
+udhcpd
+
+echo "Running ${UDHCPD_PID} ..."
+wait ${UDHCPD_PID}
